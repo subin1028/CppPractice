5b7244962aceb1047d17108f0a5633e40ea10df4
subin1028 <2lsbapple@gmail.com>
2024-05-23 16:28:50 +0900

    0.0.2s

--- solution/Array/B10808.cpp
+++ solution/Array/B10808.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string word;
 int arr[26];
 
 int main(void){
     cin.tie(0);
+    string word;
     cin >> word;
-    for (int i = 0; i < word.size(); i++){
-        arr[(int)word[i]-97] += 1;
+    for (auto e: word){
+        arr[e-'a']++;
     }
 
-    for (auto e: arr){
-        cout << e << ' ';
+    for (auto a: arr){
+        cout << a << ' ';
     }
 }
 
