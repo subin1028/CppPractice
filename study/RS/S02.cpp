8c6cda3da9bad8ed095c339738ebd77713df4045
subin1028 <2lsbapple@gmail.com>
2024-06-01 18:15:15 +0900

    Feat: add stack using STL

---
+++ study/RS/S02.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    stack<int> S;
+    S.push(10);
+    S.push(20);
+    S.push(30);
+    cout << S.size() << endl;
+    if(S.empty()) cout << "S is empty" << endl;
+    else cout << "S is not empty" << endl;
+    S.pop();
+    cout << S.top() << endl;
+    S.pop();
+    cout << S.top() << endl;
+    S.pop();
+    if(S.empty()) cout << "S is empty" << endl;
+    cout << S.top() << endl; 
+}
