4bf3998be799958d45b12775b2ac8ea545c01af8
subin1028 <2lsbapple@gmail.com>
2024-06-07 02:33:31 +0900

    0.0.1s

--- solution/RS/B1874.cpp
+++ solution/RS/B1874.cpp
@@ -2,32 +2,26 @@
 using namespace std;
 
 stack<int> S;
-vector<string> A;
 int num;
 
 int main(void){
     cin.tie(0);
     ios::sync_with_stdio(0);
     
+    string answer;
     int org = 1;
     S.push(100001);
     cin >> num;
     for(int i = 1; i <= num; i++){
         int em;
         cin >> em;
-        // if(em < org){cout << "NO"; break;}
         while(em >= org){
             S.push(org);
-            // cout << "+" << endl;
-            // cout << S.top() << endl;
-            A.push_back("+");
+            answer += "+\n";
             org++;
         }
-        if (S.top() == em) {S.pop(); A.push_back("-");}
-        else if(S.top() > em) {cout << "NO"; A.clear(); break;}
+        if (S.top() == em) {S.pop(); answer += "-\n";}
+        else if(S.top() > em) {cout << "NO"; return 0;}
     }
-
-    for(int i = 0; i < A.size(); i++){
-        cout << A[i] << endl;
-    }
+    cout << answer;
 }
