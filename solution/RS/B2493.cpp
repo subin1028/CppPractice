07b9979d52e674f93de86969358d028fd8088469
subin1028 <2lsbapple@gmail.com>
2024-06-07 01:05:51 +0900

    0.0.2f

--- solution/RS/B2493.cpp
+++ solution/RS/B2493.cpp
@@ -1,32 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define X first
+#define Y second
+
+stack<pair<int, int>> S;
+int cnt;
+
 int main(void){
-    int num;
-    int top;
-    cin >> num;
-    vector<int> V;
-    vector<int> A;
+    S.push({100000001, 0});
+    cin >> cnt;
+    for(int i = 1; i <= cnt; i++){
+        int num;
+        cin >> num;
 
-    for(int i = 0; i < num; i++){
-        int stack;
-        cin >> stack;
-        V.push_back(stack);
-    }
-
-    for(int i = num-1; i > -1; i--){
-        top = V[i];
-        if(i == 0) {A.push_back(0);break;}
-        for(int j = i-1; j > -1; j--){
-            if(top <= V[j]) {A.push_back(j+1); break;}
-            else if(j == 0) A.push_back(0);
-        }
-    }
-
-    int i = A.size()-1;
-    while(i >= 0){
-        if (i == 0) cout << A[i];
-        else cout << A[i] << ' ';
-        i--;
+        while(S.top().X < num) S.pop();
+        cout << S.top().Y << " ";
+        S.push({num, i});
     }
 }
